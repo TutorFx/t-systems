e7d9199961b20c185c4c5c26e4c6ee19bee245b3
Gabriel Serejo <gabriel_serejo@live.com>
2022-10-22 13:41:16 -0300

    Refresh page update

--- pages/[user].vue
+++ pages/[user].vue
@@ -1,11 +1,12 @@
 <template>
     <div class="content lg:mx-12 mx-3 lg:my-12 my-6">
         <h1 class="text-3xl font-extrabold text-center mb-6">Top Users</h1>
-        <div v-if="account && !pending" class="flex items-center justify-center gap-6">
+        <div v-if="account" class="flex items-center justify-center gap-6">
             <div class="flex justify-center max-w-50 mx-auto">
                 <atomUser :username="account.login" :img="account.avatar_url" />
             </div>
         </div>
+        <div v-else-if="pending">Loading...</div>
         <div v-else class="text-center">Users not found</div>
     </div>
 </template>
@@ -14,11 +15,10 @@
 const route = useRoute();
 let { pending, data: account } = useLazyAsyncData('account', () => $fetch(`https://api.github.com/users/${route.params.user}`))
 const refresh = () => refreshNuxtData('account')
+refresh({ dedupe: true })
 </script>
 <script>
 export default{
-    created(){
-        this.refresh()
-    }
+
 }
 </script>
